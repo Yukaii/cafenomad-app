245b52060f630dfbd6ff131be20ee9971276bd60
Yukai Huang <yukai@YukaideMacBook-Air.local>
2016-12-18 16:27:32 +0800

    Setup iOS hello world build

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class App extends Component {
+	render() {
+		return(
+			<View>
+				<Text>Hello World!</Text>
+			</View>
+		)
+	}
+}
