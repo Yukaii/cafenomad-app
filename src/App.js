28f78f74dcb65c66dde57d04c87adc6981e8fda5
Yukai Huang <yukaihuangtw@gmail.com>
2016-12-18 17:40:52 +0800

    Place cafe markers on Map

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
-import { View, Text, Alert, ScrollView } from 'react-native';
+import { View, Text, Alert, ScrollView, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
 
 import { getCafes } from './utils/api';
+
+const styles = StyleSheet.create({
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
 
 export default class App extends Component {
 	constructor(props) {
@@ -22,11 +33,16 @@ export default class App extends Component {
 
 	render() {
 		return(
-			<ScrollView>
-				{ this.state.cafes.map(cafe => {
-					return <Text>{JSON.stringify(cafe)}</Text>
-				}) }
-			</ScrollView>
+			<MapView
+				style={styles.map}
+			>
+				{this.state.cafes.map(cafe => (
+					<MapView.Marker
+						coordinate={{latitude: parseFloat(cafe.latitude), longitude: parseFloat(cafe.longitude)}}
+						title={cafe.name}
+					/>
+				))}
+			</MapView>
 		)
 	}
 }
