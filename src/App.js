c6f972b80370bcbdcff9f6772bd2aeedc2b19bf7
Yukai Huang <yukaihuangtw@gmail.com>
2016-12-18 22:58:48 +0800

    Fit viewport to current geolcation on start

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ const styles = StyleSheet.create({
   },
 });
 
+const SPACE = 0.003;
+
 export default class App extends Component {
 	constructor(props) {
 		super(props);
@@ -20,6 +22,12 @@ export default class App extends Component {
 	}
 
 	componentDidMount() {
+		navigator.geolocation.getCurrentPosition(position => {
+			this.map.fitToCoordinates(this.positionRect(position.coords), {
+				animated: true
+			})
+		})
+
 		getCafes().then(response => response.json()).then(cafes => {
 			this.setState({
 				cafes
@@ -27,9 +35,19 @@ export default class App extends Component {
 		})
 	}
 
+	positionRect(coords) {
+		const { latitude, longitude } = coords;
+
+		return [
+			{ latitude: latitude - SPACE, longitude: longitude - SPACE },
+			{ latitude: latitude + SPACE, longitude: longitude + SPACE },
+		];
+	}
+
 	render() {
 		return(
 			<MapView
+				ref={ref => { this.map = ref; }}
 				style={styles.map}
 			>
 				{this.state.cafes.map(cafe => (
