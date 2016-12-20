f62cf91401a7a695f4305bae2a7384ed1c610360
Yukai Huang <yukaihuangtw@gmail.com>
2016-12-20 23:15:32 +0800

    Replace hard coded zoom factor to calculated one

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Alert, ScrollView, StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 import MapView from 'react-native-maps';
 
 import { getCafes } from './utils/api';
@@ -9,8 +9,6 @@ const styles = StyleSheet.create({
 		...StyleSheet.absoluteFillObject
 	}
 });
-
-const SPACE = 0.003;
 
 export default class App extends Component {
 	constructor(props) {
@@ -38,9 +36,22 @@ export default class App extends Component {
 	positionRect(coords) {
 		const { latitude, longitude } = coords;
 
+		const { width, height } = Dimensions.get('window');
+		const viewportHeightKm = 0.7 / width * height;
+		const viewportWidthKm = 0.7;
+
+		const LONGITUDE_TO_KM = 102.08;
+		const LATITUDE_TO_KM  = 110.574;
+
 		return [
-			{ latitude: latitude - SPACE, longitude: longitude - SPACE },
-			{ latitude: latitude + SPACE, longitude: longitude + SPACE },
+			{
+				latitude: latitude + viewportHeightKm / LATITUDE_TO_KM / 2,
+				longitude: longitude - viewportWidthKm / LONGITUDE_TO_KM / 2
+			},
+			{
+				latitude: latitude - viewportHeightKm / LATITUDE_TO_KM / 2,
+				longitude: longitude + viewportWidthKm / LONGITUDE_TO_KM / 2
+			}
 		];
 	}
 
@@ -54,6 +65,7 @@ export default class App extends Component {
 					<MapView.Marker
 						coordinate={{latitude: parseFloat(cafe.latitude), longitude: parseFloat(cafe.longitude)}}
 						title={cafe.name}
+						key={cafe.id}
 					/>
 				))}
 			</MapView>
